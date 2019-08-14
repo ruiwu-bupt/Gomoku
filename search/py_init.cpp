1e7fc705982e9123ba86702cbd880551365b935b
shufenbuguadi <wogeshidabenxiong@gmail.com>
2019-08-14 22:32:50 +0800

    python3.5m

--- search/py_init.cpp
+++ search/py_init.cpp
@@ -7,13 +7,18 @@ int init() {
     if (!Py_IsInitialized()) {
         return -1;
     }
+    // PySys_SetArgv(1, argv);
+    PyRun_SimpleString("print('hello world')");
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('../cnn')");
     import_array();
+    PyRun_SimpleString("print('hello world2')");
     PyObject *pModule, *pDict, *pClass, pInstance;
     pModule = PyImport_ImportModule("resnet");
+    PyRun_SimpleString("print('hello world3')");
     pDict = PyModule_GetDict(pModule);
+    PyRun_SimpleString("print('hello world4')");
     pClass = PyDict_GetItemString(pDict, "resnet39");
-    PyRun_SimpleString("print('hello world')");
+    
     return 0;
 }
